c008ca0292386778c754e0949f8899849a8a859d
fabiod89 <40812098+fabiod89@users.noreply.github.com>
2022-03-12 14:19:49 -0500

    Add files via upload

---
+++ Reset_User_Password_AD.ps1
@@ -0,0 +1,11 @@
+﻿$first_name = Read-Host "First name"
+$last_name = Read-Host "Last name"
+$account_name = Get-ADUser -filter {GivenName -Like $first_name -and Surname -like $last_name} | Select -ExpandProperty samaccountname
+$choice = Read-Host "Would you like to reset $first_name $last_name's ($account_name) password?"
+$password = (("!@#$%^&*()1234567890qwertyuioasdzxcvbnfghrty").ToCharArray() | Get-Random -Count 12) -join ""
+
+if ($choice -eq "y"){
+
+    write-host $password
+    set-adaccountpassword -Identity $account_name -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $password -Force) 
+}
